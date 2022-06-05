0431963848e8311d4ba496b065ac5f0648f98ff2
neosork <neosork@gmail.com>
2022-06-05 17:17:12 +0300

    avadakedavra

--- decriptor/src/main/java/EncriptDecript.java
+++ decriptor/src/main/java/EncriptDecript.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class EncriptDecript {
 
     private int sizeOfString;
@@ -6,6 +8,8 @@ public class EncriptDecript {
     private static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
             'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
             'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
+
+    private static HashMap<Character, Integer> charInt = new HashMap<>();
 
 
     private int takeIndex(char c) {
@@ -57,7 +61,7 @@ public class EncriptDecript {
 
             String newString = new String(newArrayChar);
 
-            System.out.println(newString);
+           // System.out.println(newString);
 
             return newString;
         }
@@ -70,6 +74,9 @@ public class EncriptDecript {
                 newArrayChar[i] = newCharDecript(stringToChar[i], step);
             }
 
+            String newString = new String(newArrayChar);
+
+            return newString;
         }
         
 
